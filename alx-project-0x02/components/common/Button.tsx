edac73a889f2fc8e2480b274b52a698dd5e87b24
zakariabanid101 <banidzakaria2@gmail.com>
2025-06-07 02:24:48 +0100

    add reusable Button component with size and shape props

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,20 @@
+import { type ButtonProps } from '@/interfaces';
+
+const sizeClasses = {
+  small: 'px-2 py-1 text-sm',
+  medium: 'px-4 py-2 text-base',
+  large: 'px-6 py-3 text-lg',
+};
+
+const Button: React.FC<ButtonProps> = ({ size, shape, label, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`bg-blue-600 text-white ${sizeClasses[size]} ${shape} transition hover:bg-blue-700`}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,10 +1,14 @@
+import Button from "@/components/common/Button";
 import Header from "@/components/layout/Header";
 
 const About = () => {
     return (
         <>
-        <Header />
-        <div className="text-2xl font-bold">About page</div>
+            <Header />
+            <div className="text-2xl font-bold">About page</div>
+            <Button size="small" shape="rounded-sm" label="Small Button" />
+            <Button size="medium" shape="rounded-md" label="Medium Button" />
+            <Button size="large" shape="rounded-full" label="Large Button" />
         </>
     );
 };
